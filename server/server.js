b50ca7d110c9ab32914a364f035eea3161c7fb55
Sara <sameads9@gmail.com>
2022-10-03 12:41:36 -0500

    client side react app and concurrently

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 // create new Apollo server and pass in schema data
@@ -25,6 +26,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   // integrate Apollo server with the Express application as middleware
   server.applyMiddleware({app});
+
+  // serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
